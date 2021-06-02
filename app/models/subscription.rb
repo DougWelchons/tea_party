21542559b422aa33036cf0013879a634b5c477a8
DougWelchons <doug.welchons@gmail.com>
2021-06-02 11:57:54 -0600

    Add tests and validations for subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,12 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :status, presence: true
+  validates :frequency, presence: true
+
+  enum status: [:active, :canceled]
+  enum frequency: [:weekly, :biweekly, :monthly]
 end
